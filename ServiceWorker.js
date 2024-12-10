e76961beb2705dcbbb754afbc6f7dffce69a7731
OOONEPROG <unitycsnoob@gmail.com>
2024-12-10 17:47:07 +0300

    0.15 UUPPPPLOADDDD!
    wedfgbhn

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Тёмная Тайга-0.15";
+const contentToCache = [
+    "Build/015.loader.js",
+    "Build/015.framework.js.br",
+    "Build/015.data.br",
+    "Build/015.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
